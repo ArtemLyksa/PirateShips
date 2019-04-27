d18ebc129291c6817b798fed48c78f27849f7a9c
Artem <artemlyksa@gmail.com>
2019-04-27 11:19:40 +0300

    Initial commit

---
+++ PirateShips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PirateShips
+//
+//  Created by Artem Lyksa on 4/27/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
